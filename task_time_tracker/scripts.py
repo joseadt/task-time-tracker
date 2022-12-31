e8b1f86005b8cbdb3e88708ec6679b8739f25986
Jose Antonio Dosil Toja <josedosilt@gmail.com>
2022-12-31 17:51:14 +0100

    Poetry scripts

--- task_time_tracker/scripts.py
+++ task_time_tracker/scripts.py
@@ -1,7 +1,18 @@
 from PyQt6 import uic
 
+"""Poetry scripts"""
 
 def compile_ui():
     def map_filename(fileName, py_file_name):
         return fileName, "ui_" + py_file_name
     uic.compile_ui.compileUiDir("./task_time_tracker/ui", True, map_filename)
+
+def start():
+    compile_ui()
+    # Execute main script
+    from .main import start as main_start
+    main_start()
+    
+
+    
+    
